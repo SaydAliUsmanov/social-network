2c0e48eec835592392e901de0273d8935d7abc7f
Abdul <abdul.taramov2001@gmail.com>
2022-12-29 17:57:46 +0300

    Delete src/shared/ui/backdrop directory

--- src/shared/ui/backdrop/Backdrop.tsx
+++
@@ -1,8 +0,0 @@
-import { fixedFullCss } from 'shared/styles/css';
-import styled from 'styled-components';
-
-export const Backdrop = styled.div`
-  ${fixedFullCss}
-  background-color: ${({ theme }) => theme.colors.backdrop};
-  z-index: -1;
-`;
